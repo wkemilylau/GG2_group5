f8f2d44083c5286c90995e60c3216299baceb520
av124 <av639@cam.ac.uk>
2025-05-19 12:03:16 +0100

    ct_calibrate comments

--- ct_calibrate.py
+++ ct_calibrate.py
@@ -15,10 +15,12 @@ def ct_calibrate(photons, material, sinogram, scale):
 	# length (has to be the same as in ct_scan.py)
 	n = sinogram.shape[1]
 
-	#added stuff
+	#ct_calibrate additions
 	I0_E = np.sum(photons, axis=0)         
 	I0_total = np.sum(I0_E)                 
-	clipped_sinogram = np.clip(sinogram, 1e-12, None)  
+	clipped_sinogram = np.clip(sinogram, 1e-12, None) #prevents extremely small values in the sinogram, which would cause extremely large values of p, leading to instability. 
 	p = -np.log(clipped_sinogram / I0_total)           
 
 	return p
+
+
