db0c0f3fc9830a6a749d9e94e68d40fdc2180877
av124 <av639@cam.ac.uk>
2025-05-22 09:52:11 +0100

    test 1 changes

--- ct_test_example.py
+++ ct_test_example.py
@@ -18,23 +18,26 @@ source = Source()
 # all the output should be saved in a 'results' directory
 
 def test_1():
-	# explain what this test is for
+    # Checking that mean of reconstructed image is close to mean of phantom
 
-	# work out what the initial conditions should be
-	p = ct_phantom(material.name, 256, 3)
-	s = source.photon('100kVp, 3mm Al')
-	y = scan_and_reconstruct(s, material, p, 0.01, 256)
+    p = ct_phantom(material.name, 256, 3)
+    s = source.photon('100kVp, 3mm Al')
+    y = scan_and_reconstruct(s, material, p, 0.01, 256)
 
-	# save some meaningful results
-	save_draw(y, 'results', 'test_1_image')
-	save_draw(p, 'results', 'test_1_phantom')
+    save_draw(y, 'results', 'test_1_image')
+    save_draw(p, 'results', 'test_1_phantom')
 
-	# how to check whether these results are actually correct?
+    mean_p = np.mean(p)
+    mean_y = np.mean(y)
+
+    # Checking that means are reasonably close (within 1% of each other)
+    return abs(mean_p - mean_y) / mean_p < 0.01 
+
 
 def test_2():
 	# explain what this test is for
 	"""
-	Test 2 checks that a point-source phantom can be scanned and
+	Test 2 checks that a point-source phantom can be scanned and 
 	reconstructed to output something that is close to a delta function at 0
 	"""
 
@@ -50,14 +53,14 @@ def test_2():
 	peak_index = np.argmax(np.abs(y[128,:]))
 	peak_value = y[128,:][peak_index]
 
-	# check the rest of the signal is near 0 but allow for transition
+	# check the rest of the signal is near 0 but allow for transition 
 	# band around 0 due to unideal filtering
 	transition_band = np.arange(118, 138, 1)
 	rest = np.delete(y[128,:], transition_band)
 	rest_max = np.max(np.abs(rest))
 
 	# print(peak_index == 128 and rest_max < 0.2*peak_value)
-
+	
 	return peak_index == 128 and rest_max < 0.2*peak_value
 
 
@@ -67,8 +70,7 @@ def test_3():
 
 	# work out what the initial conditions should be
 	p = ct_phantom(material.name, 256, 1)
-	save_draw(p, 'results', 'test_3_phantom')
-	s = source.photon('80kVp, 1mm Al')
+	s = fake_source(source.mev, 0.1, method='ideal')
 	y = scan_and_reconstruct(s, material, p, 0.1, 256)
 
 	# save some meaningful results
@@ -77,16 +79,15 @@ def test_3():
 	f.close()
 
 	# how to check whether these results are actually correct?
-	expected_mean = 640  # hypothetical expected mean
-	actual_mean = np.mean(y[64:192, 64:192])
-	print(actual_mean)
-	assert np.isclose(actual_mean, expected_mean, rtol=0.05), f"Mean {actual_mean} differs from expected {expected_mean}"
 
 
 # Run the various tests
-#print('Test 1')
-#test_1()
-#print('Test 2')
-#print(test_2())
+print('Test 1')
+test_1()
+print('Test 2')
+print(test_2())
 print('Test 3')
 test_3()
+
+y = ct_detect(source.photon('100kVp, 2mm Al'), material.coeff('Water'), np.arange(0,10.1,0.1),1)
+plot(np.log(y))  # plotting log of the residual energy
