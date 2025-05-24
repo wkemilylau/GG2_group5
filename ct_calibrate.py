6b7841b0f95884ca0ba2ce012dfd472ede5629c1
Hana Iza Kim <hizakim@gmail.com>
2025-05-24 16:54:52 +0100

    ct_calibrate now takes into account air travel

--- ct_calibrate.py
+++ ct_calibrate.py
@@ -1,5 +1,5 @@
 import numpy as np
-import scipy 
+import scipy
 from scipy import interpolate
 
 def ct_calibrate(photons, material, sinogram, scale):
@@ -16,11 +16,19 @@ def ct_calibrate(photons, material, sinogram, scale):
 	n = sinogram.shape[1]
 
 	#ct_calibrate additions
-	I0_E = np.sum(photons, axis=0)         
-	I0_total = np.sum(I0_E)                 
-	clipped_sinogram = np.clip(sinogram, 1e-12, None) #prevents extremely small values in the sinogram, which would cause extremely large values of p, leading to instability. 
-	p = -np.log(clipped_sinogram / I0_total)           
+	I0_E = np.sum(photons, axis=0)
+	I0_total = np.sum(I0_E)
+
+	# air attenuation correction
+	air_mu = material.coeff('Air')      # mu for air (array of same shape as photons)
+	air_thickness = 2 * n * scale       # distance from source to detector through air
+
+	# apply Beer–Lambert law for each energy: I = I0 * exp(-μ*d)
+    # compute air-attenuated intensity
+	I_air = photons * np.exp(-air_mu * air_thickness)
+	I_air_total = np.sum(I_air)         # what detector would actually measure with just air
+
+	clipped_sinogram = np.clip(sinogram, 1e-12, None) #prevents extremely small values in the sinogram, which would cause extremely large values of p, leading to instability.
+	p = -np.log(clipped_sinogram / I_air_total)
 
 	return p
-
-
